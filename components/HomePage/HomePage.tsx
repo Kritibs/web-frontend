17cd90031112cf3ab1ace4651bd8188024cf2bb3
Kritib Bhattarai <kritibbhattarai@Kritibs-MacBook-Pro.local>
2023-03-19 01:58:20 -0700

    initialize nav bar

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
+import styles from "./Homepage.module.css"
 
 export default function HomePage() {
   return (
     <>
       <div className="grid grid-cols-2 gap-4 content-center">
         <div className="flex flex-col justify-evenly">
-          <div>LUTHER COMMUNITY</div>
-          <div>Services that our community needs most</div>
+          <h1 className={`${styles.lc_community} font-thin sclae-y-75 tracking-widest -mb-16 text-2xl`}>LUTHER COMMUNITY</h1>
+          <div className="font-bold text-6xl">Services that our community needs most</div>
         </div>
         <div> 
             <Image
-            className="object-contain"
+            className="object-contain rounded-full"
             src="/logo.jpg"
             alt="logo of Luther Marketplace"
             width={500}
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,12 @@
-export default function Layout({children}: {children:any}){
-    return(
-        <div className="max-w-screen-lg mx-auto my-0">
-            {children}
-        </div>
-    )
+import Nav from "../Nav/Nav";
+export default function Layout({ children }: { children: any }) {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Nav />
+        <div className="max-w-screen-lg mx-auto my-0">{children}</div>
+
+      </div>
+    </>
+  );
 }
