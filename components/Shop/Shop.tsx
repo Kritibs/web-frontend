24e7834f6d2ee8286b008fb2b0a0cffe2f2947c3
Kritib Bhattarai <kritibbhattarai@Kritibs-MacBook-Pro.local>
2023-03-24 22:52:55 -0700

    shop fixed

--- components/Shop/Shop.tsx
+++ components/Shop/Shop.tsx
@@ -1,44 +1,47 @@
-import Image from 'next/image'
 import styles from "./Shop.module.css"
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import useSWR from 'swr';
-import {fetcher} from "../../fetch/";
+import Image from 'next/image'
+// import Link from 'next/link'
+// import { useRouter } from 'next/router'
+// import useSWR from 'swr';
+// import {fetcher} from "../../fetch/";
 
 export default function Shop() {
 
   return (
     <>
-      <div className="headerimagecontainer">
-        <div className="textheader">
-          <h1 className="parentheadtext">Find out what the <br></br>community is selling</h1>
+      <div className={styles.headerimagecontainer}>
+        <div className={styles.textheader}>
+          <h1 className={styles.parentheadtext}>Find out what the <br></br>community is selling</h1>
           <br></br>
-          <h3 className="subheadtext">Your friends and professors trust Luther Marketplace everyday. <br></br> 
+          <h3 className={styles.subheadtext}>Your friends and professors trust Luther Marketplace everyday. <br></br> 
           Explore and get the goods you need for cheap.</h3>         
         </div>
-        <div className="searchbar">
+        <div className={styles.searchbar}>
           <form method="get" action="">
-            <input className="searchfield" type="text" placeholder="Search" required></input>
+            <input className={styles.searchfield} type="text" placeholder="Search" required></input>
           </form>
         </div>
 			</div>
 
-		<div className="productgrid">
-			<div className="productcard" /* key={product.id} */>
-				<img className="productimage" src="https://images.pexels.com/photos/7303738/pexels-photo-7303738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" width={500} height={500} alt="Search bar picture"/>	
-				<div className="productcontent">
+		<div className={styles.productgrid}>
+			<div className={styles.productcard} /* key={product.id} */>
+				<Image className={styles.productimage}
+				 src="https://images.pexels.com/photos/7303738/pexels-photo-7303738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+				 width={500} height={500} alt="Search bar picture"
+				 />	
+				<div className={styles.productcontent}>
 					<div>
-						<div className="productdetails">
+						<div className={styles.productdetails}>
 							<span>Product Name</span>									
 						</div>
-						<div className = "productcondition">
-							<p className = "productconditiontext">Used New</p>
+						<div className = {styles.productcondition}>
+							<p className = {styles.productconditiontext}>Used New</p>
 						</div>	
-						<div className="productbio">
+						<div className={styles.productbio}>
 							The detailed bio of the product goes here. The detailed bio of the product goes here. The detailed bio of the product goes here.
 						</div>
 					</div>
-					<div className="productprice">
+					<div className={styles.productprice}>
 						$100.00
 					</div>
 				</div>
