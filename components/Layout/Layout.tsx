7cd71bb3512a86b0fab95d82570563dda64a85c0
Kritib Bhattarai <kritibbhattarai@Kritibs-MacBook-Pro.local>
2023-05-04 21:57:43 -0500

    fix dark mode background

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 export default function Layout({ children }: { children: any }) {
   return (
     <>  
-      <div className="flex flex-col min-h-screen bg-gray-200">
+      <div className="flex flex-col min-h-screen bg-gray-200 dark:bg-black">
         <div className="max-w-screen-lg mx-auto my-0"> 
         <Nav />
          {children}
