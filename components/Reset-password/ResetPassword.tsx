0001890d7fd3636c7694962fb9e7dc288db9fb31
1935-a11ly <musami01@luther.edu>
2023-05-05 00:30:20 -0500

    update reset password UI

--- components/Reset-password/ResetPassword.tsx
+++ components/Reset-password/ResetPassword.tsx
@@ -48,24 +48,42 @@ function SetPassword({ session }: { session: any }) {
 
   return (
     <main className="">
-      <div className="">
-        <div className="">
+      <div className="flex justify-center mt-10 mb-20">
+        <div className="w-full max-w-md">
           <br></br>
-          <h1 className="">Password Reset</h1>
-          <h2 className="">Please enter your details.</h2>
-          <form>
-            <input
-              className=""
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Password"
-              required
-              onChange={handleChange}
-            />
+          <h1 className="text-2xl text-center py-2 text-gray-800 dark:text-gray-300">Password Reset</h1>
+          <h2 className="text-md text-center py-4 mb-4 text-gray-800 dark:text-gray-300">Please enter your details below 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-block animate-bounce w-6 h-6 ml-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          </h2>
+          <form className="px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                  className="block text-cyan-600 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 dark:text-gray-300 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                required
+                onChange={handleChange}
+              />
+            </div>
             <br></br>
+            <label
+                  className="block text-cyan-600 text-sm font-bold mb-2"
+                  htmlFor="password2"
+            >
+                  Confirm Password
+            </label>
             <input
-              className=""
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 dark:text-gray-300 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
               type="password2"
               id="password2"
               name="password2"
@@ -74,8 +92,8 @@ function SetPassword({ session }: { session: any }) {
               onChange={handleChange}
             />
             <br></br>
-            <button className="" type="button" onClick={handleSubmit}>
-              Send Reset Link
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSubmit}>
+              Reset Password
             </button>
           </form>
         </div>
@@ -111,17 +129,25 @@ function Email({ session }: { session: any }) {
   };
   return (
     <main className="">
-      <Link href="/">
-        <h1 className="">Luther Marketplace</h1>
-      </Link>
-      <div className="">
-        <div className="">
+      <div className="flex justify-center mt-10 mb-20">
+        <div className="w-full max-w-md">
           <br></br>
-          <h1 className="">Password Reset</h1>
-          <h2 className="">Please enter your details.</h2>
-          <form>
+          <h1 className="text-2xl text-center py-2 text-gray-800 dark:text-gray-300">Password Reset</h1>
+          <h2 className="text-md text-center py-4 mb-4 text-gray-800 dark:text-gray-300">Please enter your details below   
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline-block animate-bounce w-6 h-6 ml-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          </h2>
+          <form className="px-8 pt-2 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                  className="block text-cyan-600 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+              </label>
             <input
-              className=""
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 dark:text-gray-300 leading-tight focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
               type="text"
               id="email"
               name="email"
@@ -129,9 +155,10 @@ function Email({ session }: { session: any }) {
               required
               onChange={handleChange}
             />
+            </div>
             <br></br>
-            <button className="" type="button" onClick={handleSubmit}>
-              Send Reset Link
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSubmit}>
+              Send Reset Link 
             </button>
           </form>
         </div>
